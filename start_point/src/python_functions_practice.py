3792752380ed6584e86d4bfdce7501ea637b7c27
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-22 13:17:30 +0100

    test multiply done

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -10,3 +10,8 @@ def subtract(num_01, num_02):
     return num_01 - num_02
 
 subtract_result = subtract(10, 5)
+
+def multiply(num_01, num_02):
+    return num_01 * num_02
+
+multiply_result = multiply(4, 2)
